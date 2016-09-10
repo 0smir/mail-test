886acf3032703d2b16165e6d2ad23d2c38210c4e
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-09-10 17:08:17 +0300

    add changes in gulp-task (create folder dist)

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,29 @@ var gulp = require('gulp'),
     inky = require('inky'),
     inlineCss = require('gulp-inline-css'),
     inlinesource = require('gulp-inline-source');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    inky = require('inky'),
+    inlineCss = require('gulp-inline-css'),
+    inlinesource = require('gulp-inline-source');
 
 
 //STYLES
 gulp.task('styles', function () {
-  return gulp.src('./scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    return gulp.src('./scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 //CONVERTE INKY
 gulp.task('inky', ['styles'], function() {
-  return gulp.src('./templates/**/*.html')
-    .pipe(inlinesource())
-    .pipe(inky())
-    .pipe(inlineCss({
-        preserveMediaQueries: true
-    }))
-    .pipe(gulp.dest('./dist'));
+    return gulp.src('./templates/**/*.html')
+        .pipe(inlinesource())
+        .pipe(inky())
+        .pipe(inlineCss({
+            preserveMediaQueries: true
+        }))
+        .pipe(gulp.dest('./dist'));
 });
 
 //WATCH
