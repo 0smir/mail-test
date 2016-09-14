fc37b3e61fe8286d187dbf403451e06e48e58115
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-09-15 01:01:51 +0300

    add changs in gulp task: create three task for styles, inky, inline css

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,6 @@ var gulp = require('gulp'),
     inky = require('inky'),
     inlineCss = require('gulp-inline-css'),
     inlinesource = require('gulp-inline-source');
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    inky = require('inky'),
-    inlineCss = require('gulp-inline-css'),
-    inlinesource = require('gulp-inline-source');
 
 
 //STYLES
@@ -17,18 +12,35 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('./dist/css'));
 });
 
+//CONVERTE INKY convert to inline styles without media
+// gulp.task('inky', ['styles'], function() {
+//     return gulp.src('./templates/**/*.html')
+//         .pipe(inky())
+//         .pipe(gulp.dest('./dist'));
+// });
+
+
 //CONVERTE INKY
-gulp.task('inky', ['styles'], function() {
+gulp.task('inky', function() {
     return gulp.src('./templates/**/*.html')
-        .pipe(inlinesource())
         .pipe(inky())
-        .pipe(inlineCss({
-            preserveMediaQueries: true
-        }))
         .pipe(gulp.dest('./dist'));
 });
 
+//INLINE CSS
+gulp.task('inline', function () {
+    return gulp.src('./dist/*.html')
+        .pipe(inlineCss())
+        .pipe(gulp.dest('./dist/inlined'));
+});
+
+
 //WATCH
+// gulp.task('default',function() {
+//     gulp.watch(['./scss/*.scss', './templates/**/*.html'],['inky']);
+// });
+
 gulp.task('default',function() {
-    gulp.watch(['./scss/*.scss', './templates/**/*.html'],['inky']);
+    gulp.watch('./scss/**/*.scss',['styles']);
+    gulp.watch('./templates/**/*.html',['inky']);
 });
